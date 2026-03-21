654d181b630a18c8576aa63d6299456e2acd550b
Willy Heng <willyheng@hotmail.sg>
2026-03-21 22:57:23 +0800

    test commit msg

---
+++ src/main/java/sc2002/assignment1/testing.java
@@ -0,0 +1,9 @@
+package sc2002.assignment1;
+
+public class testing {
+
+	public static void main(String[] args) {
+        // Prints the message to the terminal window.
+        System.out.println("Hello, World!");
+    }
+}
