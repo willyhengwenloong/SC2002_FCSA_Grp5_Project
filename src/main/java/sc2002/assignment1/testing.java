10733beb59477ffe74318588c54ce86c980ff5c9
Willy Heng <willyheng@hotmail.sg>
2026-03-21 23:08:54 +0800

    Remove testing

--- src/main/java/sc2002/assignment1/testing.java
+++
@@ -1,9 +0,0 @@
-package sc2002.assignment1;
-
-public class testing {
-
-	public static void main(String[] args) {
-        // Prints the message to the terminal window.
-        System.out.println("Hello, World!");
-    }
-}
