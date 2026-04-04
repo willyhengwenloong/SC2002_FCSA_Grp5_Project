b4fe7a4c865a952f52a19d2cc65f948dad6b2b10
Willy Heng <willyheng@hotmail.sg>
2026-04-04 20:44:34 +0800

    initialise combatants

--- src/main/java/combatants/Combatants.java
+++ src/main/java/combatants/Combatants.java
@@ -1,5 +1,34 @@
 package combatants;
 
 public abstract class Combatants {
-
+	private String name;
+	private int maxHp;
+	private int currentHp;
+	private int attack;
+	private int defense;
+	private int speed;
+	private boolean alive;
+	
+	public Combatants(String name, int hp, int attack, int defense, int speed) {
+		this.name = name;
+		this.maxHp = hp;
+		this.currentHp = hp;
+		this.attack = attack;
+		this.defense = defense;
+		this.speed = speed;
+		this.alive = true;
+	}
+	
+	//getter setter
+	public String getName() {return name;}
+	public int getMaxHp() {return maxHp;}
+	public int getCurrentHp() {return currentHp;}
+	public int getAttack() { return attack;}
+	public int getDefense() {return defense;}
+	public int getSpeed() {return speed;}
+	public boolean isAlive() {return alive;}
 }
+
+
+
+
--- src/main/java/combatants/Enemy.java
+++ src/main/java/combatants/Enemy.java
@@ -1,5 +1,8 @@
 package combatants;
 
 public abstract class Enemy extends Combatants{
-
+	public Enemy(String name, int hp, int attack, int defense, int speed) {
+		super(name, hp, attack, defense, speed);
+		
+	}
 }
