818540b9713fd8c5372b4c335b4ac88d06897dfc
Willy Heng <willyheng@hotmail.sg>
2026-04-04 22:15:35 +0800

    Added Game Entry Point, Removed HelloWorld

--- src/main/java/ui/ConsoleUI.java
+++ src/main/java/ui/ConsoleUI.java
@@ -1,5 +1,17 @@
 package ui;
 
 public class ConsoleUI {
-
+	
+	private static final String LINE  = "=============================";
+	private static final String DLINE = "_____________________________";
+	
+	public static void printLoadingScreen()
+	{
+		System.out.println(LINE);
+		System.out.println(" TURN-BASED COMBAT ARENA");
+		System.out.println(LINE);
+		System.out.println();
+	}
+	
+	
 }
