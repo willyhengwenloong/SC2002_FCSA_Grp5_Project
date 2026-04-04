3fdfbcc98487828c379d1ea417d3c758468fb29f
Willy Heng <willyheng@hotmail.sg>
2026-04-04 23:20:51 +0800

    Setting up 1.0

--- src/main/java/ui/ConsoleUI.java
+++ src/main/java/ui/ConsoleUI.java
@@ -13,5 +13,19 @@ public class ConsoleUI {
 		System.out.println();
 	}
 	
+	//Game Setup CLI
+	public static void printPlayerChoices() {
+		System.out.println("Choose Your Character");
+		System.out.println(DLINE);
+		System.out.println("1. Warrior");
+		System.out.println("HP: 260 ATK: 40 DEF: 20 SPD: 30");
+		System.out.println("Special Skill: Shield Bash - Deals Damage + stuns target");
+		System.out.println();
+		System.out.println("2. Wizard");
+		System.out.println("HP: 200 ATK: 50 DEF: 10 SPD: 20");
+		System.out.println("Special Skill: Arcane Blast - AOE Damage, Kills Boost ATK");
+		System.out.println(DLINE);
+		System.out.println("Enter choice (1-2): ");
+	}
 	
 }
