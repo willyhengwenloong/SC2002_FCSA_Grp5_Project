5e29139bda8ef6d3ddf60e2595de40850f9df1cb
Uva <uvadharanyaramesh@gmail.com>
2026-04-06 17:52:56 +0800

    updated items and potions

--- src/main/java/items/Items.java
+++ src/main/java/items/Items.java
@@ -1,5 +1,9 @@
 package items;
 
 public interface Items {
-
+	
+	public String use(Player player, List<Combatant> targets);
+	public String getName();
+	public String getDescription();
+	public boolean isUsed();
 }
