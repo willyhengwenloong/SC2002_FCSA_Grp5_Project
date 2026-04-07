3c3b29da306b60da68e9f243dff65a0f8fd72aa1
Uva <uvadharanyaramesh@gmail.com>
2026-04-07 19:46:12 +0800

    added turn order function

--- src/main/java/strategy/speedBasedTurnOrderStrategy.java
+++ src/main/java/strategy/speedBasedTurnOrderStrategy.java
@@ -1,5 +1,14 @@
 package strategy;
 
-public class speedBasedTurnOrderStrategy implements TurnOrderStrategy {
+import combatants.Combatants;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
 
+public class speedBasedTurnOrderStrategy implements TurnOrderStrategy {
+	public List<Combatants> determineTurnOrder(List<Combatants> combatants){
+		List<Combatants> ordered = new ArrayList<>(combatants);
+		ordered.sort(Comparator.comparingInt(Combatants::getSpeed).reversed());
+		return ordered;
+	}
 }
