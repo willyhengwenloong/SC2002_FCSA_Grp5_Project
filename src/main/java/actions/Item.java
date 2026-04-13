06119395afbbadbe89f82cadf97df633b08106ce
Willy Heng <willyheng@hotmail.sg>
2026-04-14 00:45:10 +0800

    Updated Actions

--- src/main/java/actions/Item.java
+++ src/main/java/actions/Item.java
@@ -1,8 +1,22 @@
 package actions;
 import combatants.Combatants;
+import items.Items;
+
 public class Item implements Actions{
+	
+	private Items items; 
+	
+	public Item(Items items) {
+		this.items = items;
+	}
+	
 	@Override
     public void execute(Combatants user) {
         System.out.println(user.getName() + " uses an item.");
+        
 	}
+	
+	public String getName() { return "Item: " + items.getName();}
+	
+	public Items getItems() { return items;}
 }
